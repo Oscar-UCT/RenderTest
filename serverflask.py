831e2eb206802cdd63afb48ea9b72694b38fa813
Oscar <ocariaga2024@alu.uct.cl>
2024-11-16 17:48:32 -0300

    port update

--- serverflask.py
+++ serverflask.py
@@ -129,4 +129,5 @@ class Order():
 
 if __name__ == '__main__':
     # init_db()  # Initialize the database
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0')
